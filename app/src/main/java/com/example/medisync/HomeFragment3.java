8de3407f836159dacdef23f9b3f0fb64787ac233
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-09-21 00:45:36 +0530

    HomeFramelayout created

--- app/src/main/java/com/example/medisync/HomeFragment3.java
+++ app/src/main/java/com/example/medisync/HomeFragment3.java
@@ -1,6 +1,7 @@
 package com.example.medisync;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -11,11 +12,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link HomeFragment3#newInstance} factory method to
  * create an instance of this fragment.
- *
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment3 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,26 +26,26 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    public HomeFragment3() {
+        // Required empty public constructor
+    }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment HomeFragment3.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static HomeFragment3 newInstance(String param1, String param2) {
+        HomeFragment3 fragment = new HomeFragment3();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    public HomeFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -63,21 +63,8 @@ public class HomeFragment extends Fragment {
         FragmentManager fragmentManager = getChildFragmentManager();
 
         // Create a FragmentTransaction
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
 
-        // Add a fragment to the container (R.id.fragment_container) in your layout
-        HomeFragment_2 firstFragment = new HomeFragment_2();
-        transaction.add(R.id.fragment2, firstFragment);
-
-        // Commit the transaction
-        transaction.commit();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
-    private void replaceFragment3(Fragment fragment) {
-        FragmentManager fragmentManager = getChildFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment2, fragment);
-        fragmentTransaction.commit();
+        return inflater.inflate(R.layout.fragment_home3, container, false);
     }
 }
--- app/src/main/java/com/example/medisync/Splash.java
+++ app/src/main/java/com/example/medisync/Splash.java
@@ -2,13 +2,30 @@ package com.example.medisync;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Splash extends AppCompatActivity {
+    Button Login;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Login=findViewById(R.id.Login);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(Splash.this,Home.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
 }
