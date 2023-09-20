9479a14aaa8f094addc0daf68a74636a4e1050b9
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-09-20 22:15:31 +0530

    Home BottomNavigation

--- app/src/main/java/com/example/medisync/Home.java
+++ app/src/main/java/com/example/medisync/Home.java
@@ -5,10 +5,6 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class Home extends AppCompatActivity {
 
@@ -16,51 +12,16 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        final View mapFrame = findViewById(R.id.mapframe);
-        final View bottomSheet = findViewById(R.id.sheet);
-
-        // Get the BottomSheetBehavior from the FrameLayout
-        //final BottomSheetBehavior<View> bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-
-        // Calculate the screen height
-        final int screenHeight = getResources().getDisplayMetrics().heightPixels;
-
-        // Calculate the height for the first FrameLayout (75% of screen height)
-        final int firstFrameHeight = (int) (screenHeight * 0.75);
-
-        // Calculate the height for the second FrameLayout (25% of screen height)
-
-
-        // Set the height for the first FrameLayout
-        ViewGroup.LayoutParams mapFrameParams = mapFrame.getLayoutParams();
-        mapFrameParams.height = firstFrameHeight;
-        mapFrame.setLayoutParams(mapFrameParams);
-
-
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         // Create a FragmentTransaction
         FragmentTransaction transaction = fragmentManager.beginTransaction();
 
         // Add a fragment to the container (R.id.fragment_container) in your layout
-        MapsFragment firstFragment = new MapsFragment();
-        transaction.add(R.id.mapframe, firstFragment);
+        HomeFragment firstFragment = new HomeFragment();
+        transaction.add(R.id.homeFrame, firstFragment);
 
         // Commit the transaction
         transaction.commit();
-        //View bottomSheet = findViewById(R.id.sheet);
-
-// Get the BottomSheetBehavior from the FrameLayout
-        BottomSheetBehavior<View> bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-        int peakHeightPixels = (int) (screenHeight * 0.29); // Replace with your desired value
-        bottomSheetBehavior.setPeekHeight(peakHeightPixels);
-
-        // Set the initial state of the BottomSheet to collapsed
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-
     }
-
-    // You can define methods to handle fragment transactions based on user interactions
-
 }
-
