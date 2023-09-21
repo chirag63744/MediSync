e406a4d0522622ced47146a755c07bfa66fca2e5
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-09-22 01:38:54 +0530

    Image added

--- app/src/main/java/com/example/medisync/Login.java
+++ app/src/main/java/com/example/medisync/Login.java
@@ -6,13 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    TextView t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        t1=findViewById(R.id.textView8);
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, Register.class);
+                startActivity(i);
+            }
+        });
 
         Button b1 = (Button) findViewById(R.id.button);
         b1.setOnClickListener(new View.OnClickListener() {
